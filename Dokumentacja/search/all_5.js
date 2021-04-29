fbf5331892c997c7215111d0448063863447f7de
JMag-repo <80643081+JMag-repo@users.noreply.github.com>
2021-04-29 16:04:18 +0200

    Add files via upload

---
+++ Dokumentacja/search/all_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['feedview_31',['FeedView',['../classplatformy___n_e_t_1_1_m_w_m_1_1_view_1_1_feed_view.html#a921796da5f429cfaf70577355f8f1661',1,'platformy_NET.MWM.View.FeedView.FeedView()'],['../classplatformy___n_e_t_1_1_m_w_m_1_1_view_1_1_feed_view.html',1,'platformy_NET.MWM.View.FeedView']]],
+  ['feedviewmodel_32',['FeedViewModel',['../classplatformy___n_e_t_1_1_m_w_m_1_1_view_model_1_1_feed_view_model.html',1,'platformy_NET::MWM::ViewModel']]],
+  ['followers_33',['Followers',['../classplatformy___n_e_t_1_1_models_1_1_spot_search_1_1_followers.html',1,'platformy_NET::Models::SpotSearch']]]
+];
---
+++ Dokumentacja/search/all_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['homeview_42',['HomeView',['../classplatformy___n_e_t_1_1_m_w_m_1_1_view_1_1_home_view.html#a7bf42c87b82a0ec86709bab30108e8ae',1,'platformy_NET.MWM.View.HomeView.HomeView()'],['../classplatformy___n_e_t_1_1_m_w_m_1_1_view_1_1_home_view.html',1,'platformy_NET.MWM.View.HomeView']]],
+  ['homeviewmodel_43',['HomeViewModel',['../classplatformy___n_e_t_1_1_m_w_m_1_1_view_model_1_1_home_view_model.html',1,'platformy_NET::MWM::ViewModel']]]
+];
